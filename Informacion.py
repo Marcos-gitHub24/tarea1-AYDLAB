0eb6dec916a4357d37d016e2a031fab410f5a0a9
Marcos <3004263640101@ingenieria.usac.edu.gt>
2022-06-05 00:14:08 -0600

    Se agrego la informacion del auxliar en el endpoint "informacion-aux"

--- Informacion.py
+++ Informacion.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def add_guide():
     return str("Jose Marcos García Olmino - 201903895")
 
+@app.route('/informacion-aux', methods=["GET"])
+def add_inforAux():
+    return str("Auxiliar de AyD: Manuel de Mata")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
     app.run(debug=True)
